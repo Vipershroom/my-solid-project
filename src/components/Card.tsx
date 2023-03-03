53cd52b9ee87bcdf85cce1f3fadf813d84c398de
Vipershroom <undertalefan812@gmail.com>
2023-03-03 13:27:05 -0500

    Add partial card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,9 +2,12 @@ import { Component } from "solid-js";
 
 const Card: Component = () => {
   
-  return <div class='flex mt-10 w-1/2'>
-    <h1>Term 1</h1>
-    <h1>Term 2</h1>
+  return <div class='flex mt-10 w-1/2 justify-between
+  border border-solid border-black rounded-md shadow-lg'>
+    <div>
+        <h1>Jello</h1>
+    </div>
+    <div />
   </div>;
 };
 
