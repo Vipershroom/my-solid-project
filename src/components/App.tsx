9456e953bd6ff88aad44ab6c21b9ba2eac75b09e
Vipershroom <undertalefan812@gmail.com>
2023-03-03 14:16:07 -0500

    Add arrow and Card

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,18 @@
-import type { Component } from 'solid-js';
-import Card from './Card';
-import Header from './Header';
-import MainCard from './MainCard/MainCard';
+import type { Component } from "solid-js";
+import Card from "./Card";
+import Header from "./Header";
+import MainCard from "./MainCard/MainCard";
 
 const App: Component = () => {
   return (
-    <div class='bold text-3xl h-screen'>
+    <div class="bold text-3xl h-screen">
       <Header />
-      <div class='flex flex-col items-center mt-10'>
+      <div class="flex items-center justify-center mt-10">
+        <p>&lt;</p>
         <MainCard />
+        <p>&gt;</p>
+      </div>
+      <div class="flex flex-col items-center mt-10">
         <Card />
       </div>
     </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,20 @@
 import { Component } from "solid-js";
 
 const Card: Component = () => {
-  
-  return <div class='flex mt-10 w-1/2 justify-between
-  border border-solid border-black rounded-md shadow-lg'>
-    <div>
-        <h1>Jello</h1>
+  return (
+    <div class="flex mt-10 w-1/2 border border-solid border-black justify-between rounded-md shadow-lg">
+      <div class="w-1/2 text-2xl p-2 border-r-black border-r-2">
+        <h2>Lorem ipsum dolor sit amet.</h2>
+      </div>
+      <div class="w-1/2 text-2xl flex justify-end p-2">
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat minus
+          recusandae autem!
+        </h2>
+      </div>
+      <div />
     </div>
-    <div />
-  </div>;
+  );
 };
 
 export default Card;
